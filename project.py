4c0d9622896d565ddef8e517ca0dacce0a061485
fault <critical@fault.io>
2016-02-05 16:53:49 -0700

    Note as project and add module.

---
+++ project.py
@@ -0,0 +1,12 @@
+name = 'console'
+fork = 'xterm'
+identity = 'https://fault.io/project/python/console'
+abstract = 'fault.io based editor and shell'
+icon = ''
+release = None
+
+controller = 'fault.io'
+contact = 'mailto:critical@fault.io'
+
+version_info = (0, 0, 0)
+version = '.'.join(map(str, version_info))
