3dad7ae8dc5a666ceb117b95c4ac422ac19c8a8a
fault <critical@fault.io>
2019-03-13 00:54:21 -0700

    Correct routers

--- lines/rust.py
+++ lines/rust.py
@@ -81,7 +81,7 @@ exowords = {y: y for y in map(fields.String, exoword_list)}
 
 terminators = {y: y for y in map(fields.Constant, ";")}
 separators = {y: y for y in map(fields.Constant, ",")}
-routers = {y: y for y in map(fields.Constant, ("::", ".","-",">"))} # "->"; needs special handling
+routers = {y: y for y in map(fields.Constant, (":", ".","-",">"))} # "->" "::"
 operators = {y: y for y in map(fields.Constant, "@!&^*%+=-|\\/<>?~:")}
 groupings = {y: y for y in map(fields.Constant, "()[]{}")}
 quotations = {y: y for y in map(fields.Constant, ('"',))}
