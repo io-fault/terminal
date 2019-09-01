88913f96c188525eb2bb36089fe86188410f5364
fault <critical@fault.io>
2019-09-01 12:37:13 -0700

    Temporarily include some fault.context.tools as exowords.

--- lines/python.py
+++ lines/python.py
@@ -7,6 +7,7 @@ from .. import fields
 
 keywords = {y: y for y in map(fields.String, keyword.kwlist)}
 cores = {y: y for y in map(fields.String, __builtins__.keys())}
+exowords = {y: y for y in map(fields.String, ['cachedproperty', 'cachedcalls'])}
 
 terminators = {y: y for y in map(fields.Constant, ":;#")}
 separators = {y: y for y in map(fields.Constant, ",")}
