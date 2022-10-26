32e1d210a55e4f0d6e3ea3c2ae30dc82bab31cf7
fault <critical@fault.io>
2022-10-25 17:16:03 -0700

    Highlight comethod.

--- lines/python.py
+++ lines/python.py
@@ -7,7 +7,7 @@ from .. import fields
 
 keywords = {y: y for y in map(fields.String, keyword.kwlist)}
 cores = {y: y for y in map(fields.String, __builtins__.keys())}
-exowords = {y: y for y in map(fields.String, ['cachedproperty', 'cachedcalls'])}
+exowords = {y: y for y in map(fields.String, ['cachedproperty', 'cachedcalls', 'comethod'])}
 
 terminators = {y: y for y in map(fields.Constant, ":;#")}
 separators = {y: y for y in map(fields.Constant, ",")}
